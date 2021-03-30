d37c5b47412ae2fa51890d834a3633417b5cb97b
runnern <runner-0003@mail.ru>
2021-03-30 13:50:04 +0300

    1.0.0

--- main.cpp
+++ main.cpp
@@ -5,75 +5,74 @@
 #include <cstdlib>
 #include <string>
 #include <omp.h>
+#include <fstream>
 
 using namespace std;
 
 // количество кубитов
-#define N 4
+#define COUNT_QUBIT 20
 // номер кубита , по которому проводится преобразование
 #define NUM_QUBIT 1
 
-// возвращает двоичное представление числа num длиной len
-string getBinary(int number, int len) {
-	string result;
-	while (number > 0) {
-		if (number % 2 == 0)
-			result = '0' + result;
-		else
-			result = '1' + result;
-		number /= 2;
-		len--;
-	}
-	while (len--)
-		result = '0' + result;
-	return result;
+// проверяет на 0 или 1 бит на numQubit-ном месте числа number
+int checkBit(unsigned long long number, int numQubit) {
+	unsigned long long mask = 1 << (COUNT_QUBIT - numQubit);
+	if ((number & mask) == 0)
+		return 0;
+	return 1;
 }
 
-// возвращает число в 10-ой с.с. , которое получится после постановки 0 или 1 
-// на numQubit-ное место в 2-ое представление числа number
-int putZeroOrOne(char zeroOrOne, string &number, int numQubit) {
-	number[numQubit - 1] = zeroOrOne;
-	int numberNew = 0;
-	for (int i = 0; i < number.length(); i++)
-		if (number[i] == '1')
-			numberNew += pow(2, number.length() - i - 1);
-	return numberNew;
+// возвращает число в 10-ой с.с. , которое получится после постановки 0 или 1 на numQubit-ный бит числа number
+unsigned long long putZeroOrOne(int zeroOrOne, unsigned long long number, int numQubit) {
+	unsigned long long mask = 1 << (COUNT_QUBIT - numQubit);
+	if (zeroOrOne == 1)
+		return (number | mask);
+	mask = ~mask;
+	return (number & mask);
 }
 
 // функция однокубитного преобразования
 vector <complex <double> > oneQubitTransformation(vector <complex <double> > &vec, vector <vector <complex <double> > > &matrix, int numQubit) {
 	vector <complex <double> > vecTransformed(vec.size());
 	#pragma omp parallel for
-	for (int i = 0; i < vec.size(); i++) {
-		string iTwoBit = getBinary(i, log(vec.size()) / log(2));
-		vecTransformed[i] = matrix[iTwoBit[numQubit] - '0'][0] * vec[putZeroOrOne('0', iTwoBit, numQubit)] +
-			matrix[iTwoBit[numQubit] - '0'][1] * vec[putZeroOrOne('1', iTwoBit, numQubit)]; 
-	}
+	for (long long i = 0; i < vec.size(); i++)
+		vecTransformed[i] = matrix[checkBit(i, numQubit)][0] * vec[putZeroOrOne(0, i, numQubit)] +
+			matrix[checkBit(i, numQubit)][1] * vec[putZeroOrOne(1, i, numQubit)]; 
 	return vecTransformed;
 }
 
-int	main() {
+int	main(int argc, char **argv) {
+	cout << sizeof(complex<float>) << endl;
+	cout << sizeof(complex<double>) << endl;
+	cout << sizeof(float) << endl;
+	cout << sizeof(double) << endl;
 	// инициализация вектора случайными значениями
-	int vectorSize = pow(2, N);
+	unsigned long long vectorSize = pow(2, COUNT_QUBIT);
 	vector <complex <double> > vec(vectorSize);
 	#pragma omp parallel for
-	for (int i = 0; i < vec.size(); i++)
+	for (long long i = 0; i < vec.size(); i++)
 		vec[i] = complex<double> (1. / rand(), 1. / rand());
 	// инициализация матрицы преобразования Адамара
 	vector <vector <complex <double> > > matrix(2);
 	matrix[0].resize(2);
 	matrix[1].resize(2);
 	#pragma omp parallel for collapse(2)
-	for (int i = 0; i < 2; i++)
-		for (int j = 0; j < 2; j++) {
+	for (long long i = 0; i < 2; i++)
+		for (long long j = 0; j < 2; j++) {
 			matrix[i][j] = 1. / sqrt(2);
 			if (i == 1 && j == 1)
 				matrix[i][j] = - matrix[i][j];
 		}
 	// вызов функции однокубитного преобразования
+	double begin = omp_get_wtime();
 	vector <complex <double> > vecTransformed = oneQubitTransformation(vec, matrix, NUM_QUBIT);
+	double end = omp_get_wtime();
+	// вывод времени работы программы в файл
+	ofstream fout(argv[1], ios_base::app);
+	fout << end - begin << endl;
+	fout.close();
 	// вывод нового вектора
-	for (int i = 0; i < vecTransformed.size(); i++)
-		cout << vecTransformed[i] << endl;
+	// for (long long i = 0; i < vecTransformed.size(); i++)
+	// 	cout << vecTransformed[i] << endl;
 	return 0;
 }
